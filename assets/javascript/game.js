b1f11a5c29f9df30e744e2f7e117a2773b161b70
Mad Tea <43210154+nosidam48@users.noreply.github.com>
2018-11-14 21:36:22 -0500

    Initial setup and a little pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//create question objects with arrays of potential answers
+var 
+//create a function that causes the element to display on the page regardless of which question 
+
+//create a function that only allows the player to click on one button. 
+//possibly in the same function, compare userGuess to the correct answer.
+
+//create a timer that starts on a start button and starts again everytime there is a new question
+
+//if correct answer, then show win screen for x seconds (use timer), if incorrect, show loss screen w/ correct answer, if time runs out, show time ran out with correct answer for x seconds.
+
+//create a timer that tell how long to hold on the win screen
+
+//
